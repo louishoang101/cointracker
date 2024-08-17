7818466fa3a61f3c2d059706361ef9b7295afce7
Luie Hoang <luieee@192-168-1-183.tpgi.com.au>
2024-08-17 22:54:40 +1000

    add

---
+++ CoinTracker.py
@@ -0,0 +1,42 @@
+import requests, json 
+import pandas as pd
+from bs4 import BeautifulSoup
+import time,datetime
+from datetime import datetime
+import os
+from twilio.rest import Client
+import pendulum
+
+#Set up twilio account via API
+account_sid = 'ACd59de4b7ea746ef5cbf1e68d7087ef19'
+auth_token = '87c4c5069b3a671e72e462b48689d1d3'
+
+client = Client(account_sid, auth_token)
+
+from_whatsapp_number = 'whatsapp:+14155238886'
+to_whatsapp_number = 'whatsapp:+61431515817'
+
+#scrape Greed&Fear Index
+gf_url = 'https://alternative.me/crypto/fear-and-greed-index/'
+gf_response = requests.get(gf_url)
+gf_soup = BeautifulSoup(gf_response.content, 'html.parser')
+
+gf_box = gf_soup.find('div', class_ = 'fng-value')
+gf_index_today = gf_box.find('div', class_ = 'fng-circle').get_text()
+gf_status_today = gf_box.find('div', class_ = 'status').get_text()
+gf_time_today = gf_box.find('div', class_ = 'gray').get_text()
+
+#scrape AltCoin Season Index
+alt_url = 'https://www.blockchaincenter.net/en/altcoin-season-index/'
+alt_response = requests.get(alt_url)
+alt_soup = BeautifulSoup(alt_response.content, 'html.parser')
+
+alt_box = alt_soup.find('button', class_ = 'nav-link timeselect active')
+alt_index_today = alt_box.find('b').get_text()
+
+gf_message = "The greed and fear index today is " + gf_index_today + ", which is " + gf_status_today
+alt_message = "\n < 25 being bitcoin and > 75 being altcoin." + " Today's index is " + alt_index_today
+
+client.messages.create(body = gf_message + alt_message,
+                   from_ = from_whatsapp_number,
+                   to = to_whatsapp_number)
---
+++ GeckoAPI.py
@@ -0,0 +1,54 @@
+
+import requests
+import json 
+import pendulum
+import time
+from datetime import datetime
+
+from twilio.rest import Client
+
+#Set up twilio account via API
+account_sid = 'ACd59de4b7ea746ef5cbf1e68d7087ef19'
+auth_token = '87c4c5069b3a671e72e462b48689d1d3'
+
+client = Client(account_sid, auth_token)
+
+from_whatsapp_number = 'whatsapp:+14155238886'
+to_whatsapp_number = 'whatsapp:+61431515817'
+
+headers = {
+"accept": "application/json",
+"x-cg-demo-api-key": "CG-2XuLUW2Yg2tQhe1hYogFg3mY"
+}
+url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&price_change_percentage=1h"
+
+response = requests.get(url, headers=headers)
+coin_list = response.json()
+
+#FORMULA
+formula = "SYMBOL:CURR:24H:1H"
+
+#GETTING BTC
+btc = f"{str(coin_list[0]['symbol']).upper()}:{coin_list[0]['current_price']}:{coin_list[0]['price_change_percentage_24h']}:{coin_list[0]['price_change_percentage_1h_in_currency']}"
+
+filtered_coins = [coin for coin in coin_list if 200000000 < coin["market_cap"] < 2000000000]
+
+sorted_coins = sorted(filtered_coins, key=lambda x:x["price_change_percentage_24h"], reverse=True)
+
+top_coins = sorted_coins[:20]
+list_midcap = []
+for coin in top_coins:
+    list_midcap.append(f'{coin["symbol"].upper()}:{round(coin["price_change_percentage_24h"],2)}:{round(coin["price_change_percentage_1h_in_currency"],2)}')
+
+filtered_coins_sc = [coin for coin in coin_list if 20000000 < coin["market_cap"] <= 200000000]
+
+sorted_coins_sc = sorted(filtered_coins_sc, key = lambda x:x["price_change_percentage_24h"], reverse=True)
+top_coins_sc = sorted_coins_sc[:20]
+list_smallcap = []
+for coin in top_coins_sc:
+    list_smallcap.append(f'{coin["symbol"].upper()}:{round(coin["price_change_percentage_24h"],2)}:{round(coin["price_change_percentage_1h_in_currency"],2)}')
+
+#return "Here are the list of top 20 coins (MID-CAP) with most 24h change: \n" + str(list_midcap)+ '\n' + "\nHere are the list of top 20 coins (SMALL-CAP) with most 24h change: \n" + str(list_smallcap)
+client.messages.create(body = formula + "\n\n" + btc + "\n\n" + "Here is the list of top 20 coins (MID-CAP) with most 24h change: \n" + str(list_midcap)+ '\n' + "\nHere is the list of top 20 coins (SMALL-CAP) with most 24h change: \n" + str(list_smallcap),
+                    from_ = from_whatsapp_number,
+                    to = to_whatsapp_number)
